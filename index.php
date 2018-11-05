38a45c62932e8f9f5d491c13b7232d55101a967a
Pedro Penha <pedro.penha.martins@gmail.com>
2018-11-05 15:55:47 -0300

    Adicionando campo de mensagens

--- index.php
+++ index.php
@@ -64,7 +64,18 @@ if(r === true) window.location.href='sair.php';" style="float: right">
         </div>
         <br/>
         <br/>
-
+        <h3>Mensagens</h3>
+        <div class="accordion" id="accordionExample">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Collapsible Group Item #1
+                        </button>
+                    </h5>
+                </div>
+            </div>
+        </div>
     </div>
     </body>
     </html>
